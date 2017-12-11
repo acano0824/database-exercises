35049d34a5ca444c083da245d2f874e36321a088
ashleycano <acano6835@gmail.com>
2017-12-11 10:29:25 -0600

    solution for select exercise

---
+++ select_exercise.sql
@@ -0,0 +1,13 @@
+use codeup_test_db;
+
+SELECT name FROM albums where name = 'Pink Floyd';
+
+SELECT release_date FROM albums where name = 'Sgt. Pepper\'s Lonely Hearts Club';
+
+SELECT genre FROM albums where name = 'Nevermind';
+
+SELECT * FROM albums where release_date between '1990' and '2000';
+
+SELECT * FROM albums where sales < '20000000';
+
+SELECT * FROM albums where genre = 'Rock';
